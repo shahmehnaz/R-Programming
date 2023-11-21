948735395f1bf3c3b3c04887f3d7b2ddd0f9ef83
shahmehnaz <69505586+shahmehnaz@users.noreply.github.com>
2023-11-21 12:23:06 -0500

    Add files via upload

---
+++ operators in r programming.R
@@ -0,0 +1,97 @@
+#operators used in r 
+#arithmetic operator
+#comparison/relational operator
+#logical operator
+#assignment operator
+#membership operator(special operators)
+#miscellanous operator(special operators)
+
+int1=20
+int2=70
+
+#arithmetic operators
+
+#addition(+)
+int3=int1+int2
+int3
+
+#subbtraction(-)
+int4=int2-int1
+
+#multiplication(*)
+int5=int1*int2
+int5
+
+#division(/)
+int6=int2/int1
+int6
+
+#power of/exponention(^)
+int7=int2^int1
+int7
+
+#modular division(remainder)(%%)
+int8=int2%%int2
+int8
+
+#integer division(quotient)(a%/%b)
+int9=int2%/%int1
+int9
+
+#assignment operators
+int10=50  #simple assignment(=)
+int11<-60 #leftward assignment(<-)
+70-> int12  #rightward assignment(->)
+
+#comparison operator
+
+#equal to operator(==)
+int12==20
+
+#not equal to operator(!=)
+int12!=20
+
+#less than operator(<)
+int10<int12
+
+#greater than operator(>)
+int10>int12
+
+#less than or equal to(<=)
+int10<=int12
+
+#greater than or equal to(>=)
+int10>=int12
+
+#logical operator
+
+#NOT operator(!)
+log1=TRUE
+log2=FALSE
+!log2
+
+#AND  operator(& or &&)
+log1&log2
+
+#OR operator(| or||)
+log1|log2
+
+#membership operator
+#to check if element belongs to a vector(a%in%b)
+numbers=c(1,2,3,4,5)
+2%in%numbers
+8%in%numbers
+
+#miscellaneous operators
+#to create sequence of numbers(:)
+1:10
+
+#matrix multiplication(a%*%b)
+matrix1=c(1,2,3,4,5)
+matrix2=c(1,1,1,1,1)
+matrix3=matrix1%*%matrix2
+matrix3
+
+
+
+
