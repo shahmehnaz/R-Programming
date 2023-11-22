fbfbca9b8b718cf4a0e3f3de8949dda26f8dcc31
shahmehnaz <69505586+shahmehnaz@users.noreply.github.com>
2023-11-22 12:11:09 -0500

    Add files via upload

---
+++ if else condition in r.R
@@ -0,0 +1,20 @@
+#keywords in R Programming
+ 
+ #if-else control flow 
+  x=11
+  y=12
+  z=11
+ 
+  if(x==y && x==z)
+  { 
+    print("TRUE")
+    }else{
+      print("FALSE")
+    }
+  if(x!=y || x==z)
+  {
+    print("TRUE")
+  }else{
+    print("false")
+  }
+  
